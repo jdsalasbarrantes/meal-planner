f1fded891b5075bb96664782e65de72f6d7f7df7
Josue <jdsalasbarrantes@gmail.com>
2020-04-14 20:26:53 -0600

    Base react project

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Router } from 'react-router-dom';
+function App() {
+  return (
+    <div>
+      hola
+    </div>
+  );
+}
+
+export default App;
